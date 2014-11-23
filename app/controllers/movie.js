725a8b09bfb933a0b9074363e5cdfcfd618f9526
shannonnachreiner2012@u.northwestern.edu <shannonnachreiner2012@u.northwestern.edu>
2014-11-22 20:38:51 -0600

    stuff

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -3,7 +3,7 @@ var movieApp = angular.module('movieApp', ['MovieModel']);
 
 // Index: http://localhost/views/recipe/index.html
 
-movieApp.controller('IndexCtrl', function ($scope, RecipeRestangular) {
+movieApp.controller('IndexCtrl', function ($scope, MovieRestangular) {
 
   // Helper function for opening new webviews
   $scope.open = function(id) {
@@ -12,8 +12,8 @@ movieApp.controller('IndexCtrl', function ($scope, RecipeRestangular) {
   };
 
   // Fetch all objects from the local JSON (see app/models/recipe.js)
-  RecipeRestangular.all('movie').getList().then(function(recipes){
-    $scope.recipes = recipes;
+  RecipeRestangular.all('movie').getList().then(function(movies){
+    $scope.movies = movies;
   });
 
   // -- Native navigation
@@ -27,7 +27,7 @@ movieApp.controller('IndexCtrl', function ($scope, RecipeRestangular) {
 movieApp.controller('ShowCtrl', function ($scope, $filter, RecipeRestangular) {
 
   // Fetch all objects from the local JSON (see app/models/recipe.js)
-  RecipeRestangular.all('movie').getList().then( function(recipes) {
+  RecipeRestangular.all('movie').getList().then( function(movies) {
     // Then select the one based on the view's id query parameter
     $scope.recipe = $filter('filter')(recipes, {recipe_id: steroids.view.params['id']})[0];
   });
